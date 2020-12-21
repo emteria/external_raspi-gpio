803ceca472823a877f9333bd2bca93956ac3b70c
Igor Kalkov <ik@emteria.com>
2020-12-21 23:12:02 +0100

    Added Android makefile and notice files

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := raspi-gpio
+LOCAL_SRC_FILES := raspi-gpio.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXCECUTABLES)
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_EXECUTABLE)
