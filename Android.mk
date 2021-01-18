c3f52737bac6727cd114979a52fbaa404b44f97c
Igor Kalkov <ik@emteria.com>
2021-01-18 22:39:22 +0100

    Allow compilation for Android 11

--- Android.mk
+++ Android.mk
@@ -7,4 +7,9 @@ LOCAL_SRC_FILES := raspi-gpio.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXCECUTABLES)
 LOCAL_VENDOR_MODULE := true
 
+LOCAL_CFLAGS := \
+    -Wno-sign-compare \
+    -Wno-unused-parameter \
+    -Wno-unused-variable \
+
 include $(BUILD_EXECUTABLE)
